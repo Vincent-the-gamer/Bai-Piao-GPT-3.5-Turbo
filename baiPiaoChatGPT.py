a0aa7e8645affd73ca17c2541b6eee39c4d9f8d1
vincent-the-gamer <1127620453@qq.com>
2023-04-08 15:12:28 +0800

    修正README.md

--- baiPiaoChatGPT.py
+++ baiPiaoChatGPT.py
@@ -51,9 +51,6 @@ def send_message(content: str):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     }
 
-    # 生成sign哈希值
-
-
     json = {
         "messages": messages,
         "pass": None,
